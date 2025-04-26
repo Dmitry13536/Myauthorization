266c88a477f4b28b6aec7dfe8e37c3eca1ee32ea
Dmitry13536 <sarychev2k06@gmail.com>
2025-04-26 15:16:18 +0300

    feat registration is done

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -7,50 +7,54 @@ import { useMyNavigate } from "../useMyNavigate";
 const Login = ({ data }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [alert, setAlert] = useState('');
 
   const NavigateTo = useMyNavigate();
 
   const tryToLogIn = () => {
-    let flag = false; //флаг для определения наличия емаила
-    data.forEach((element) => {
-      if (email === element.email) {
-        flag = true; //емаил есть
-      }
-    });
-    if (!flag) {
-      console.log("ban");
+    setAlert('');
+    if(!email || !password){setAlert("Fill all the fields"); return null;}
+    console.log(alert);
+    const user = data.find((user) => user.email === email);
+    console.log(user);
+    if (!user) {
+      setAlert('Incorrect login or password');
       return null;
     }
-    const targetItem = data.find((item) => item.email === email);
-    if (targetItem.password === Number(password)) {
-        NavigateTo(`/${targetItem.id}`);
+    if (user.password === password) {
+      NavigateTo(`/${user.id}`);
     } else {
-      console.log("ban");
+      setAlert('Incorrect login or password');
     }
   };
 
   return (
-    <div className="login">
-      <p className="login__title">Log in</p>
-      <div className="login__inputs">
-        <MyInput
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <MyInput
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+    <>
+      <div className="login">
+        <p className="login__title">Log in</p>
+        <div className="login__inputs">
+          {alert && <p className="m-10 alert">{alert}</p>}
+          <MyInput
+            required
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <MyInput
+            required
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <MyButton onClick={tryToLogIn}>Start</MyButton>
+        <span className="link">
+          <Link to="/signup">Sign up</Link>
+        </span>
       </div>
-      <MyButton onClick={tryToLogIn}>Start</MyButton>
-      <span className="link">
-        <Link to="/signup">Sign up</Link>
-      </span>
-    </div>
+    </>
   );
 };
 
--- src/Components/Settings.jsx
+++ src/Components/Settings.jsx
@@ -3,16 +3,21 @@ import MyButton from "../UI/button/MyButton";
 import { useMyNavigate } from "../useMyNavigate";
 import { useParams } from "react-router-dom";
 
-const Settings = () => {
+const Settings = ({del}) => {
     const NavigateTo = useMyNavigate();
     const {id} = useParams();
+
+    const deleteUser = () => {
+      NavigateTo('/');
+      del(Number(id))
+    }
 
   return (
     <div className="settings">
       <p className="settings__title">Settings</p>
       <div className="settings__options">
         <MyButton onClick={()=> NavigateTo('/')} colors={true} className="m-10 bg-grey">Log out</MyButton>
-        <MyButton onClick={()=> NavigateTo('/')} colors={true} className="bg-red">Delete accaunt</MyButton>
+        <MyButton onClick={deleteUser} colors={true} className="bg-red">Delete accaunt</MyButton>
       </div>
       <MyButton onClick={()=> NavigateTo(`/${id}`)}>Back</MyButton>
     </div>
